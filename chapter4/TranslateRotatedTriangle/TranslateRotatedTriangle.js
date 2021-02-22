545fda771ddede62f39a9f5c809986610dfe4439
geek1116 <1035214549@qq.com>
2021-02-22 15:57:57 +0800

    upload translaterotatedtriangle

---
+++ chapter4/TranslateRotatedTriangle/TranslateRotatedTriangle.js
@@ -0,0 +1,61 @@
+const VSHADER_SOURCE = `
+    attribute vec4 a_Position;
+    uniform mat4 u_ModelMatrix;
+    void main() {
+        gl_Position = u_ModelMatrix * a_Position;
+    }
+    `,
+    FSHADER_SOURCE = `
+        void main() {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    `
+
+const canvas = document.querySelector('#canvas'),
+    gl = canvas.getContext('webgl'),
+    ANGLE = 45.0
+
+initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)
+
+const n = initVertexBuffers()
+if(n < 0) {
+    alert('Failed to set the positions of the vertices')
+}
+
+const modelMatrix = new  Matrix4()
+modelMatrix.setTranslate(0.5, 0, 0)
+modelMatrix.rotate(ANGLE, 0, 0, 1)
+
+const u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix')
+gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements)
+
+gl.clearColor(0, 0, 0, 1.0)
+gl.clear(gl.COLOR_BUFFER_BIT)
+
+gl.drawArrays(gl.TRIANGLES, 0, n)
+
+
+function initVertexBuffers() {
+    const vertices = new Float32Array([
+        0.0, 0.3, -0.3, -0.3, 0.3, -0.3
+    ]), num = 3
+
+    // 创建缓冲区
+    const vertexBuffer = gl.createBuffer()
+
+    // 将缓冲区对象绑定到目标
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+
+    // 向缓冲区对象中写入数据
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+
+    const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
+
+    // 将缓冲区对象分配给a_Position变量
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0)
+
+    // 连接a_Position变量与分配给它的缓冲区对象
+    gl.enableVertexAttribArray(a_Position)
+
+    return num
+}
