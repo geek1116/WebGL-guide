1b6f811dc4550235b67dc5276341d965a6db131f
geek1116 <1035214549@qq.com>
2021-05-13 22:35:08 +0800

    upload look_at_triangles and look_at_triangles_with_keys

---
+++ chapter7/LookAtTriangles/LookAtTriangles.js
@@ -0,0 +1,69 @@
+const VSHADER_SOURCE = `
+    attribute vec4 a_Position;
+    attribute vec4 a_Color;
+    uniform mat4 u_ViewMatrix;
+    varying vec4 v_Color;
+    void main() {
+        gl_Position = u_ViewMatrix * a_Position;
+        v_Color = a_Color;
+    }
+`
+const FSHADER_SOURCE = `
+    precision mediump float;
+    varying vec4 v_Color;
+    void main() {
+        gl_FragColor = v_Color;
+    }
+`
+const canvas = document.querySelector('#canvas'),
+    gl = canvas.getContext('webgl')
+
+initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)
+
+const n = initVertexBuffers()   // 设置顶点坐标和颜色
+if(n < 0) {
+    alert('initial vertex fail')
+}
+
+const u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix')
+const viewMatrix = new Matrix4()
+viewMatrix.setLookAt(0.25, 0.25, 0.25, 0, 0, 0, 0, 1, 0)
+gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements)
+
+gl.clearColor(0, 0, 0, 1.0)
+gl.clear(gl.COLOR_BUFFER_BIT)
+gl.drawArrays(gl.TRIANGLES, 0, n)
+
+
+function initVertexBuffers() {
+    const verticesColors = new Float32Array([
+        0.0, 0.5, 0, 0, 1.0, 0.4,
+        -0.5, -0.5, 0, 0, 1.0, 0.4,
+        0.5, -0.5, 0, 1.0, 0.4, 0.4,
+        
+        0.5, 0.5, 0.2, 1.0, 0.4, 0.4,
+        -0.5, 0.5, 0.2, 1.0, 1.0, 0.4,
+        0.0, -0.5, 0.2, 1.0, 1.0, 0.4,
+
+        0.0, 0.5, 0.4, 0.4, 0.4, 1.0,
+        -0.5, -0.5, 0.4, 0.4, 0.4, 1.0,
+        0.5, -0.5, 0.4, 1.0, 0.4, 0.4
+    ]), n = 9
+
+    const vertexColorbuffer = gl.createBuffer()
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorbuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW)
+
+    const FSIZE = verticesColors.BYTES_PER_ELEMENT
+
+    const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, FSIZE * 6, 0)
+    gl.enableVertexAttribArray(a_Position)
+
+    const a_Color = gl.getAttribLocation(gl.program, 'a_Color')
+    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3)
+    gl.enableVertexAttribArray(a_Color)
+
+    return n
+}
